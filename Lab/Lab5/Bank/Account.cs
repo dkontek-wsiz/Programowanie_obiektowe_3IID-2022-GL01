2e7359c66aa9d9a9ee8229612369051929cb6b44
dkontek <dkontek@RA117A-01>
2023-12-06 12:32:45 +0100

    lab 5 zad 1

---
+++ Lab/Lab5/Bank/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5.Bank
+{
+    public class Account
+    {
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+        public static double Income { get; set; }
+        public static double Expense { get; set; }
+    }
+}
---
+++ Lab/Lab5/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab5.Bank;
+using Lab5.Shapes;
+using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zad1();
+        }
+
+        static void Zad1()
+        {
+            Shape shape = new Circle(5);
+
+            shape = new Square() { X = 2 };
+
+            Console.WriteLine(shape.CalulateArea());
+        }
+    }
+}
