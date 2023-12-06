5dbdaf6cc33afa60f837b78ae461dc2d6eac4c77
dkontek <dkontek@RA117A-01>
2023-12-06 13:57:50 +0100

    lab 6 zad 1

---
+++ Lab/Library/Repositories/MockBookRepository.cs
@@ -0,0 +1,43 @@
+﻿using Library.Interfaces;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Repositories
+{
+    public class MockBookRepository : IBookRepository
+    {
+        private List<Book> data = new List<Book>() { new Book() { Title = "Tesr", Id = 1} };
+
+        public void Create(Book item)
+        {
+            data.Add(item);
+        }
+
+        public List<Book> GetAll()
+        {
+            return data;
+        }
+
+
+        public void Delete(int id)
+        {
+            data.RemoveAll(x => x.Id == id);
+        }
+
+        public Book Get(int id)
+        {
+            return data.Find(x => x.Id == id);
+        }
+
+       
+        public void Update(Book item)
+        {
+            var index = data.FindIndex(x => x.Id == item.Id);
+            
+            if (index > -1)
+                data[index] = item;
+        }
+    }
+}
---
+++ Lab/Library/Repositories/MockPersonRepository.cs
@@ -0,0 +1,43 @@
+﻿using Library.Interfaces;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Repositories
+{
+    public class MockPersonRepository : IPersonRepository
+    {
+        private List<Person> data = new List<Person>();
+
+        public void Create(Person item)
+        {
+            data.Add(item);
+        }
+
+        public List<Person> GetAll()
+        {
+            return data;
+        }
+
+
+        public void Delete(long id)
+        {
+            data.RemoveAll(x => x.Id == id);
+        }
+
+        public Person Get(long id)
+        {
+            return data.Find(x => x.Id == id);
+        }
+
+
+        public void Update(Person item)
+        {
+            var index = data.FindIndex(x => x.Id == item.Id);
+
+            if (index > -1)
+                data[index] = item;
+        }
+    }
+}
